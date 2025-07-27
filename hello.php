b41c723dce36f6ef9b0fe9872ba074ac14c477b4
Ved0123 <167901907+Ved0123@users.noreply.github.com>
2025-07-27 17:33:42 +0530

    4

--- hello.php
+++ hello.php
@@ -1,2 +1,9 @@
 Try Github for the first time!
-ertcrvtbhnj
+<?php
+echo "Hello, World!";
+?>
+<?php
+// This is a simple PHP script to demonstrate a basic output
+// It prints "Hello, World!" to the screen
+// You can run this script on a server with PHP installed
+?>
