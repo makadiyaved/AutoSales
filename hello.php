8f9d3562191bd08020fce96c3a5b5f4f9279d4c2
Ved0123 <167901907+Ved0123@users.noreply.github.com>
2025-07-27 17:31:11 +0530

    1

--- hello.php
+++ hello.php
@@ -2,4 +2,3 @@ Try Github for the first time!
 <?php
 // hello.php
 echo "<h1>Hello, World!</h1>";
-dxcyatugyiwijo;rpthglmcmv nbxzhcaTQ2F3Y49UE5YRTHFLG,.M,NFDFMEAGYW3R4U8O5YKRHTMLN, DSEFVSEFYGURJITHOMLGKNC BFSDERTRGTHUIHYGMNK VCBSBFDESTRHIJKGN C
