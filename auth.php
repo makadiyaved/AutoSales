8d180eb25b24344215a4cdd40168a360a20446c4
Ved0123 <167901907+makadiyaved@users.noreply.github.com>
2025-10-31 11:53:11 +0530

    31-10-2015

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php
-// if (session_status() === PHP_SESSION_NONE) {
-//     session_start();
-// }
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once 'config/database.php';
 
 // Function to check and handle remember me cookies
