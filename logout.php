530004db204e256cd7106788b24bc4fee6e9ed8e
Ved0123 <167901907+makadiyaved@users.noreply.github.com>
2025-11-03 10:57:46 +0530

    03-11-2025

--- logout.php
+++ logout.php
@@ -1,5 +1,24 @@
 <?php
-// Redirect to login page
-header("Location: login.php");
+session_start();
+
+// Destroy all session data
+session_unset();
+session_destroy();
+
+// Clear session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+// Clear remember me cookies
+if (isset($_COOKIE['remember_token'])) {
+    setcookie('remember_token', '', time()-3600, '/', '', true, true);
+}
+if (isset($_COOKIE['user_id'])) {
+    setcookie('user_id', '', time()-3600, '/', '', true, true);
+}
+
+// Redirect to login page with success message
+header("Location: login.php?message=logged_out");
 exit();
 ?> 
